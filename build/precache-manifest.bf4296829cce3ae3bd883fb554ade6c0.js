8d340adb22289f9f7a9829af96dbe78974a149c6
AncaEmilia <AncaEmilia@users.noreply.github.com>
2020-10-16 10:00:37 +0300

    adding project

---
+++ build/precache-manifest.bf4296829cce3ae3bd883fb554ade6c0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fabff40652466ba282e75033e93dc18f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "939c29a12718c22b5acf",
+    "url": "/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "fd1d2883dece8d8e57b1",
+    "url": "/static/css/main.3b7d0291.chunk.css"
+  },
+  {
+    "revision": "939c29a12718c22b5acf",
+    "url": "/static/js/2.e7e24cc0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e7e24cc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd1d2883dece8d8e57b1",
+    "url": "/static/js/main.a3c05eb2.chunk.js"
+  },
+  {
+    "revision": "af1ee15f3b8733767315",
+    "url": "/static/js/runtime-main.8a8ac9cd.js"
+  },
+  {
+    "revision": "f0d91b088780334cb367ea8a43e70540",
+    "url": "/static/media/Adventure Time Logo.f0d91b08.woff"
+  }
+]);
---
+++ src/robots.js
@@ -0,0 +1,29 @@
+export const robots = [
+    {
+        id: 1,
+        name: 'Anca Trifanov',
+        username: 'Anca',
+        email: 'ancatrifanov@gmail.com'
+    },
+    
+    {
+            id: 2,
+        name: 'Teodor Cazangiu',
+        username: 'Teo',
+        email: 'cazangiu.teodor@gmail.com'
+    },
+    
+    {
+        id: 3,
+        name: 'Maia ',
+        username: 'Maiuc',
+        email: 'maiuc@gmail.com'
+    },
+
+    {
+        id: 4,
+        name: 'Sasa ',
+        username: 'Sosonc',
+        email: 'soson@gmail.com'
+    }
+]
