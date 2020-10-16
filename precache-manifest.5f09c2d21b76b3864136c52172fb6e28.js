e4f2ef14ffadcb588f4494308a9974b1f34016f5
AncaEmilia <AncaEmilia@users.noreply.github.com>
2020-10-16 10:16:04 +0300

    Updates

---
+++ precache-manifest.5f09c2d21b76b3864136c52172fb6e28.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06fd3cb31b902bcee395016917c40922",
+    "url": "/RoboFriends/index.html"
+  },
+  {
+    "revision": "abcf402c9619777cd68e",
+    "url": "/RoboFriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "dc91d3e7f2eec3ea0cbc",
+    "url": "/RoboFriends/static/css/main.ef9a6f68.chunk.css"
+  },
+  {
+    "revision": "abcf402c9619777cd68e",
+    "url": "/RoboFriends/static/js/2.cab51068.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/RoboFriends/static/js/2.cab51068.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc91d3e7f2eec3ea0cbc",
+    "url": "/RoboFriends/static/js/main.8753c02d.chunk.js"
+  },
+  {
+    "revision": "671d1b49487ff131bdad",
+    "url": "/RoboFriends/static/js/runtime-main.16920bcc.js"
+  },
+  {
+    "revision": "e40bd40ea4397becaa4b8b297d77e0bd",
+    "url": "/RoboFriends/static/media/Bilderberg-X3red.e40bd40e.woff"
+  }
+]);
